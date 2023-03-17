8ce55ffdef2a5eae3e26f3d11299210754c8f29e
Stas Arsoba <s@onestar.dev>
2023-03-17 10:23:58 +0100

    Change scheme url

--- client/codegen.ts
+++ client/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
  
  const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://api.thegraph.com/subgraphs/name/arsoba/stakestar-test",
+  schema: "http://139.162.184.96:8000/subgraphs/name/stakestar-testnet/graphql",
   documents: "src/queries",
    generates: {
      'sdk/index.ts': {
