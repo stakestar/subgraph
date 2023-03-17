d27c0c8125a6813a63c3996322f7b7becf1a909e
Stas Arsoba <s@onestar.dev>
2023-03-17 11:37:04 +0100

    Fix scheme url

--- client/codegen.ts
+++ client/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
  
  const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://subgraph.stakestar.io/subgraphs/name/stakestar-testnet/graphql",
+  schema: "https://subgraph.stakestar.io/subgraphs/name/stakestar-testnet",
   documents: "src/queries",
    generates: {
      'sdk/index.ts': {
